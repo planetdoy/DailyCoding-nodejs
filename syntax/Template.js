c981945d4aee16a68c4feef7c99073f9fbe20721
planetdoy <doydoit@gmail.com>
2020-11-29 00:04:03 +0900

    practice stuff

---
+++ syntax/Template.js
@@ -0,0 +1,10 @@
+var name = 'doy';
+
+var letter = 'Dear ' +name+ '\n\nLorem ipsum dolor '+name+' sit amet, consectetur adipisicing elit, '+name+' sed do eiusmod tempor incididunt '+name+' ut labore et dolore magna aliqua. '+name+' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id '+name+' est laborum.';
+
+
+var letter2 = `Dear ${name}
+
+Lorem ipsum dolor ${name} sit amet, consectetur adipisicing elit, ${name} sed do eiusmod tempor incididunt ${name} ut labore et dolore magna aliqua. ${name} Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id ${name} est laborum.${name}`;
+
+console.log(letter2);
